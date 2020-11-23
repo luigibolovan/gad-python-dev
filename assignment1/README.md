17a44107a8f45fc16212561648cae7d269fc1a47
Luigi Bolovan <luigibolovan@gmail.com>
2020-11-23 11:35:58 +0200

    added first assignment

---
+++ assignment1/README.md
@@ -0,0 +1,13 @@
+# Exercitii
+
+1. declararea unei liste care să conțină elementele ```7, 8, 9, 2, 3, 1, 4, 10, 5, 6``` (în această ordine).
+
+2. afișarea unei alte liste ordonată ```ascendent``` (lista inițială trebuie păstrată în aceeași formă)
+
+3. afișarea unei liste ordonată ```descendent``` (lista inițială trebuie păstrată în aceeași formă)
+
+4. afișarea numerelor pare din listă (folosind **DOAR** slice, altă metodă va fi considerată invalidă)
+
+5. afișarea numerelor impare din listă (folosind **DOAR** slice, altă metodă va fi considerată invalidă)
+
+6. afișarea elementelor multipli ai lui ```3```.
---
+++ assignment1/main.py
@@ -0,0 +1,27 @@
+### Assignment 1
+
+# declararea unei liste care să conțină elementele 7, 8, 9, 2, 3, 1, 4, 10, 5, 6 (în această ordine)
+initial_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+print("Initial list: {0}".format(initial_list))
+
+# afișarea unei alte liste ordonată ascendent (lista inițială trebuie păstrată în aceeași formă)
+sorted_list = sorted(initial_list)
+print("Sorted list: {0}".format(sorted_list))
+
+# afișarea unei liste ordonată descendent (lista inițială trebuie păstrată în aceeași formă)
+descending_list = sorted(initial_list, reverse=True)
+print("Descending: {0}".format(descending_list))
+
+# afișarea numerelor pare din listă (folosind DOAR slice, altă metodă va fi considerată invalidă)
+print("Even: {0}".format(sorted_list[1::2]))
+
+# afișarea numerelor impare din listă (folosind DOAR slice, altă metodă va fi considerată invalidă)
+print("Odd: {0}".format(sorted_list[::2]))
+
+# afișarea elementelor multipli ai lui 3.
+_multiples = []
+for i in initial_list:
+    if i % 3 == 0:
+        _multiples.append(i)
+
+print("multiples of 3 in the initial list: {0}".format(_multiples))
